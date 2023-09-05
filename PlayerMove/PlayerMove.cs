a03b7cdd4e805b5ca61b193e2b13cd93f8fffecf
Matheus Veras <99488005+MrVeGGi3@users.noreply.github.com>
2023-09-05 14:29:24 -0300

    Update PlayerMove.cs

--- PlayerMove/PlayerMove.cs
+++ PlayerMove/PlayerMove.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {    
     public Rigidbody2D rb; // declara a rigidbody do player;
-    [SerializeField] public float moveSpeed; // controla a velocidade do personagem;
+    public float moveSpeed; // controla a velocidade do personagem;
     private Vector2 moveInput; //Vetor para movimentação do personagem;
     private Vector2 mouseInput;//Vetor para movimentação da câmera do personagem;
-    [SerializeField]public float mouseSensitivity = 1f; //Sensitividade do Mouse;
+    public float mouseSensitivity = 1f; //Sensitividade do Mouse;
+    public Transform viewCam;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +25,11 @@ public class PlayerMove : MonoBehaviour
         Vector3 moveHorizontal = transform.up * -moveInput.x; //cria um vetor de três dimensões para que o player, ao rodar de direção, mude a referência de onde está o lado dele; "-" indica oposto do lado;    
         Vector3 moveVertical = transform.right * moveInput.y; //Idem, para o outro eixo;
         rb.velocity = (moveHorizontal + moveVertical) * moveSpeed; //Set a velocidade do player;
+        //movimento do mouse
+        mouseInput = new Vector2 (Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z - mouseInput.x);
+        viewCam.localRotation = Quaternion.Euler(viewCam.rotation.eulerAngles + new Vector3(0f, mouseInput.y, 0f);
+        
 
     }
 }
